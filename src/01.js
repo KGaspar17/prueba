932a61fd424ebcfc25519b85dd32c1333d5ee3c4
Kevin Gaspar <2122300448@soy.utj.edu.mx>
2023-09-20 19:03:01 -0600

    Practica 1

---
+++ src/01.js
@@ -0,0 +1,13 @@
+const nombre = 'KEVIN'
+const apellido = 'GASPAR GONZALEZ'
+
+const nombreCompleto = nombre + ' '+ apellido
+console.log(nombreCompleto)
+
+const nombreCompleto2 = ` HOLA
+
+
+
+MUNDO `
+
+console.log(nombreCompleto2);
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const nombre = 'Kevin'
+const apellido = 'Gaspar'
+// const numero = 1234567890
+
+let valorDado = 22
+// valorDado = 4
+
+if(true){
+    let valorDado = 6
+    console.log(valorDado)
+}
+console.log(nombre,apellido, "=" , valorDado )
